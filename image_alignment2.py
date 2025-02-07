a572cac03d817c37a64cf0ccf0bed7a07fa589a0
richardfeng-30 <richardqfeng@gmail.com>
2025-02-06 19:28:13 -0800

    merge images

--- image_alignment2.py
+++ image_alignment2.py
@@ -1,25 +1,27 @@
-from PIL import Image, ImageChops
+from PIL import Image
 
-im = Image.open("images/im2.jpg")
+im = Image.open("images/im1.jpg")
 print("size", im.size)
 
 # im.show()
 
-third = im.size[1]/3
+width, height = im.size[0], im.size[1]
+third = height // 3
 
 blue = im.crop((0, 0, im.size[0], third + 20))
-green = im.crop((0, third, im.size[0], third * 2 + 20))
-red = im.crop((0, third * 2, im.size[0], third * 3))
+green = im.crop((0, third - 10, im.size[0], third * 2 + 10))
+red = im.crop((0, third * 2 - 10, im.size[0], third * 3))
 
-# blue.show()
-# green.show()
-# red.show()
+blue.show()
+green.show()
+red.show()
 
-target_size = 30
-x = (400 - target_size) / 2
-y = (340 - target_size) / 2
+target_size = 20
+x = (width - target_size) // 2
+y = (third - target_size) // 2
+print(x, y)
 target_area = blue.crop((x, y, x + target_size, y + target_size))
-# target_area.show()
+target_area.show()
 
 def pixel_intensity(image):
     width, height = image.size
@@ -31,7 +33,7 @@ def pixel_intensity(image):
         row = []
         for x in range(width):
             r, g, b = pixels[x, y]
-            row.append((r + g + b) / 3)  # convert to grayscale
+            row.append(r)
         intensity.append(row)
     
     return intensity
@@ -43,7 +45,7 @@ red_intensity = pixel_intensity(red)
 def find_offset(target, full_image):
     target_h, target_w = len(target), len(target[0])
     image_h, image_w = len(full_image), len(full_image[0])
-    min_difference = 100000000
+    min_difference = float('inf')
     dx, dy = -1, -1
     for i in range(image_h - target_h):
         for j in range(image_w - target_w):
@@ -56,6 +58,7 @@ def find_offset(target, full_image):
             if total_difference < min_difference:
                 min_difference = total_difference
                 dx, dy = j, i
+    print(dx, dy)
     return dx, dy
 
 dx_g, dy_g = find_offset(target_intensity, green_intensity)
@@ -67,3 +70,9 @@ dy_r -= y
 
 print(f"green offset: ({dx_g}, {dy_g})")
 print(f"red offset: ({dx_r}, {dy_r})")
+
+green_aligned = green.crop((dx_g, dy_g, dx_g + blue.width, dy_g + blue.height))
+red_aligned = red.crop((dx_r, dy_r, dx_r + blue.width, dy_r + blue.height))
+
+merged = Image.merge("RGB", (blue, green_aligned, red_aligned))
+merged.show()
